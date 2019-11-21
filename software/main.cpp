9da8eda106f4c31da77fde6c05f5424652fbc775
aerobautics <aerobautics@gmail.com>
2019-11-21 04:38:52 -0800

    Added software directory

---
+++ software/main.cpp
@@ -0,0 +1,13 @@
+/* File: main.cpp
+ * Date: November 11, 2019
+ * Author: Stewart Nash
+ * Description: This is a placeholder for the initial SHA-256 algorithm
+ */
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+	
+	std::cout << "Hello, World!" << std::endl;
+	
+	return 0;
+}
